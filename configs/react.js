bf5b9a4543393db7d5e6c5ff2856bd615d6864e2
Jon Haddow <jon@haddow.me>
2025-06-29 20:48:05 +0100

    Fix react package

--- configs/react.js
+++ configs/react.js
@@ -1,40 +1,30 @@
 const tseslint = require("typescript-eslint");
-const globals = require("globals");
 const reactPlugin = require("eslint-plugin-react");
-const hooksPlugin = require("eslint-plugin-react-hooks");
+const reactHooks = require("eslint-plugin-react-hooks");
 const jsxA11y = require("eslint-plugin-jsx-a11y");
 
 module.exports = tseslint.config({
   // Entry for 'all files' (that we care about)
   files: ["**/*.{js,jsx,ts,tsx}"],
 
-  // React plugin
-  ...reactPlugin.configs.all,
+  extends: [
+    // React plugin
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat["jsx-runtime"],
 
-  // React hooks plugin
-  ...hooksPlugin.configs.recommended,
+    // React hooks plugin
+    reactHooks.configs["recommended-latest"],
 
-  // JSX a11y plugin
-  ...jsxA11y.flatConfigs.recommended,
+    // JSX a11y plugin
+    jsxA11y.flatConfigs.strict,
+  ],
 
   rules: {
     // Disabling rule as prop type can be inferred from the
     // type parameter provided in TypeScript
     "react/prop-types": "off",
-  },
 
-  languageOptions: {
-    ...reactPlugin.configs.all.languageOptions,
-    ...jsxA11y.flatConfigs.recommended.languageOptions,
-
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    globals: {
-      ...globals.browser,
-    },
+    "react/jsx-curly-brace-presence": "error",
   },
 
   settings: {
